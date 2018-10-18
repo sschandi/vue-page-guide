3bebc9d17807e4c65535a6a1b933c41d15c46f0e
chandi <sukhdeep.29@live.ca>
2018-10-18 14:59:32 -0700

    Demo works

--- docs/.vuepress/components/Demo.vue
+++ docs/.vuepress/components/Demo.vue
@@ -1,11 +1,76 @@
 <template>
-	<div>
-		<ClientOnly>
-			<h1>Testing custom component</h1>
-			<p page-guide="hello" page-guide-placement="bottom">{{ msg }}</p>
-			<button @click="showGuide = !showGuide">Show Guide</button>
-			<PageGuide v-model="showGuide"/>
-		</ClientOnly>
+	<div id="demo">
+		<div page-guide="Click anywhere on the overlay to exit" page-guide-placement="right"/>
+		<h1>Demo</h1>
+		<button
+			class="base-btn"
+			@click="showGuide = !showGuide"
+		>
+			Show Guide
+		</button>
+		<div class="demo-container">
+			<div 
+				page-guide="Target and raise entire elements"
+				page-guide-placement="top"
+				class="demo-item"
+			>
+				<div class="demo-sidebar">
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+				</div>
+				<div class="demo-page-content">
+					<div class="demo-header">
+						<h3>Header Text</h3>
+						<button class="base-btn">login</button>
+					</div>
+					<div class="demo-page-text">
+						<h4>More Text</h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere asperiores architecto nostrum, fugiat excepturi incidunt eveniet provident vitae optio impedit alias eos neque illo veniam temporibus consectetur sit corporis.</p>
+					</div>
+				</div>
+			</div>
+			<div class="demo-item">
+				<div
+					page-guide="Hightlight important features"
+					class="demo-sidebar"
+				>
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+					<p>link</p>
+				</div>
+				<div class="demo-page-content">
+					<div class="demo-header">
+						<h3>Header Text</h3>
+						<button
+							page-guide="Don't forget to login"
+							class="base-btn"
+						>
+							login
+						</button>
+					</div>
+					<div class="demo-page-text">
+						<h4>More Text</h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio facere asperiores architecto nostrum, fugiat excepturi incidunt eveniet provident vitae optio impedit alias eos neque illo veniam temporibus consectetur sit corporis.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<button
+			page-guide="This guide is explicitly to the left"
+			page-guide-placement="left"
+			class="base-btn absolute-btn"
+			@click="clickTest"
+		>
+			BTN
+		</button>
+		<PageGuide v-model="showGuide"/>
 	</div>
 </template>
 
@@ -22,13 +87,74 @@ export default {
 	},
 	data() {
 		return {
-			msg: 'Wow Message',
 			showGuide: false,
 		}
 	},
+	methods: {
+		clickTest() {
+			console.log('click')
+		}
+	}
 }
 </script>
 
 <style>
-
+#demo {
+	text-align: center;
+}
+.demo-container {
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+}
+.demo-item {
+	margin: 2rem;
+	width: 80%;
+	display: flex;
+	box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+}
+.demo-sidebar {
+	background-color: #3eaf7c;
+	color: #fff;
+	width: 20%;
+}
+.demo-page-content {
+	display: flex;
+	flex-direction: column;
+	width: 80%;
+	text-align: left;
+	background-color: #fff;
+}
+.demo-header {
+	background-color: #2c3e50;
+	color: #fff;
+	padding: 0.5rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+.demo-page-text {
+	padding: 0.5rem;
+}
+.base-btn {
+	color: #fff;
+	background-color: #3eaf7c;
+	font-size: 1.2rem;
+	padding: 0.8rem 1.6rem;
+	border-radius: 4px;
+	transition: background-color 0.1s ease;
+  box-sizing: border-box;
+	border: none;
+  border-bottom: 1px solid #389d70;
+	cursor: pointer;
+}
+.base-btn:hover {
+	background-color: #4abf8a;
+}
+.absolute-btn {
+	position: fixed;
+	right: 0;
+	bottom: 0;
+	margin: 1rem;
+}
 </style>
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,13 @@
 ---
 home: true
-actionText: View documentation
-actionLink: /guide/
+actionText: View Demo
+actionLink: /demo/
+features:
+- title: Awesome Plugin
+  details: Minimal setup with markdown-centered project structure helps you focus on writing.
+- title: Fully Customizable
+  details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
+- title: Simple to Use
+  details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
 footer: Created by SSChandi | MIT Licensed
 ---
-# Currently under development/testing
